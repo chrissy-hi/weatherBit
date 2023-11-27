1cd232ec6f73f2e716a306006437c26c8f1326bb
christina <christinapersaud98@gmail.com>
2023-11-26 23:42:27 -0800

    Add files via upload

--- weatherBitAPI.py
+++ weatherBitAPI.py
@@ -1,4 +1,6 @@
 from json import JSONDecodeError
+from typing import Optional
+
 import requests
 import json
 from fastapi import FastAPI
@@ -25,6 +27,20 @@ def current_weather(city_current):
         return "error", 500
 
 
+@app.get('/current/{lon}/{lat}')
+def current_weather(lon: str = "", lat: str = ""):
+    api_url = f'http://api.weatherbit.io/v2.0/current?key=732710630ccd492a9c1042a8b2fff513&units=I' \
+              f'&lat={lat}' \
+              f'&lon={lon}'
+    print(api_url)
+    response = requests.get(api_url)
+    try:
+        data = json.loads(response.text)
+        return data
+    except Exception as e:
+        return "error", 500
+
+
 @app.get('/forecast/{city_forecast}')
 def forecast_daily(city_forecast):
     if city_forecast.isdigit():
@@ -33,6 +49,19 @@ def forecast_daily(city_forecast):
     else:
         api_url = f'http://api.weatherbit.io/v2.0/forecast/daily?key=732710630ccd492a9c1042a8b2fff513&units=I&days=7&' \
                   f'city={city_forecast}'
+    response = requests.get(api_url)
+    try:
+        data = json.loads(response.text)
+        return data
+    except Exception as e:
+        return "error", 500
+
+
+@app.get('/forecast/{lon}/{lat}')
+def forecast_daily(lon, lat):
+    api_url = f'http://api.weatherbit.io/v2.0/forecast/daily?key=732710630ccd492a9c1042a8b2fff513&units=I&days=7&' \
+              f'&lat={lat}' \
+              f'&lon={lon}'
     response = requests.get(api_url)
     try:
         data = json.loads(response.text)
